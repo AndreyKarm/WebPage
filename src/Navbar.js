9d116e010f9b0de785b443fa5a0229765dac3aa9
AndreyKarm <karmandreii@gmail.com>
2023-10-24 17:17:49 +0100

    updates

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
         <span className="font-link">
             <nav className="nav">
                 <Link to="/" className="site-title">
-                    Undersite Inc.
+                    Goofy Inc.
                 </Link>
                 <ul>
                     <CustomLink to="/pricing">Pricing</CustomLink>
